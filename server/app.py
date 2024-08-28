e9da20576fdc567668fe8d586d6ba92ebea7a785
Chandler Abril <abrilchandler1@gmail.com>
2024-08-27 17:27:29 -0700

    Done

--- server/app.py
+++ server/app.py
@@ -22,6 +22,44 @@ def index():
 
 # Add views here
 
+# The view should query the database to get the earthquake with that id, and return a response containing the model attributes and values (id, location, magnitude, year) formatted as an JSON string. 
+# The response should include an error message if no row is found. Don't forget to import Earthquake from the models module.
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = {'id': earthquake.id,
+                'magnitude': earthquake.magnitude,
+                'location': earthquake.location,
+                'year': earthquake.year}
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+# The view should query the database to get all earthquakes having a magnitude greater than or equal to the parameter value, 
+# and return a JSON response containing the count of matching rows along with a list containing the data for each row.
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquake_dict = {'id': earthquake.id,
+                            'magnitude': earthquake.magnitude,
+                            'location': earthquake.location,
+                            'year': earthquake.year}
+        earthquakes.append(earthquake_dict)
+    body = {"count": len(earthquakes),
+                'quakes': earthquakes
+            }
+
+
+    return make_response(body, 200)
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
